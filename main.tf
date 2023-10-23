0fe4113bbbe92cf4c2ce3a4caa7c3bc145ce49e5
Chris Lea <chris.lea@gmail.com>
2023-10-23 14:50:29 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,74 @@
+# Create a VPC we'll call the "10" VPC, with public and private
+# subnets, and an IGW that the public subnets can use to route
+# to the outside Intenet.
+module "the_10_vpc" {
+  source          = "git@github.com:chrislea/example-terraform-vpc.git?ref=df1579880f27f65cd693e9e82e8145b502a3fdbb"
+  vpc_name        = "the_10_vpc"
+  main_cidr_block = "10.213.0.0/16"
+
+  public_subnet_cidrs = [
+    "10.213.0.0/19",
+    "10.213.32.0/19",
+    "10.213.64.0/19"
+  ]
+
+  private_subnet_cidrs = [
+    "10.213.96.0/19",
+    "10.213.128.0/19",
+    "10.213.160.0/19"
+  ]
+
+  igw_name = "vpc_10_igw"
+}
+
+# Create a VPC we'll call the "172" VPC, with public and private
+# subnets, and an IGW that the public subnets can use to route
+# to the outside Intenet.
+module "the_172_vpc" {
+  source          = "git@github.com:chrislea/example-terraform-vpc.git?ref=df1579880f27f65cd693e9e82e8145b502a3fdbb"
+  vpc_name        = "the_172_vpc"
+  main_cidr_block = "172.213.0.0/16"
+
+  public_subnet_cidrs = [
+    "172.213.0.0/19",
+    "172.213.32.0/19",
+    "172.213.64.0/19"
+  ]
+
+  private_subnet_cidrs = [
+    "172.213.96.0/19",
+    "172.213.128.0/19",
+    "172.213.160.0/19"
+  ]
+
+  igw_name = "vpc_172_igw"
+}
+
+# This creates a "Peering Connection", which is a gateway / router, between
+# these two VPCs.
+resource "aws_vpc_peering_connection" "vpc_peer" {
+  peer_vpc_id = module.the_172_vpc.vpc_id
+  vpc_id      = module.the_10_vpc.vpc_id
+
+  tags = {
+    Name = "VPC Peering Example"
+  }
+
+  auto_accept = true
+}
+
+# Add a route in the route table for the "10" VPC that sends any traffic
+# headed to the "172" VPC CIDR range to the Peering Connection gateway.
+resource "aws_route" "route_10_to_172" {
+  route_table_id            = module.the_10_vpc.route_table_id
+  destination_cidr_block    = module.the_172_vpc.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peer.id
+}
+
+# Add a route in the route table for the "172" VPC that sends any traffic
+# headed to the "10" VPC CIDR range to the Peering Connection gateway.
+resource "aws_route" "route_172_to_10" {
+  route_table_id            = module.the_172_vpc.route_table_id
+  destination_cidr_block    = module.the_10_vpc.vpc_cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.vpc_peer.id
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "region" {
+  type        = string
+  description = "The AWS Region to work in"
+  default     = "us-west-2"
+}
